98247bb289702f289aebaca04938393a57138360
Robert Lu <robertlu86@gmail.com>
2025-11-22 10:09:57 +0800

    PR test message

--- main.go
+++ main.go
@@ -26,6 +26,7 @@ func main() {
 	fmt.Println("WARNING: This is a development server. Do not use it in a production deployment. Use a production WSGI server instead.")
 	fmt.Println(" * Running on http://127.0.0.1:5500")
 	fmt.Println("Press CTRL+C to quit")
+	fmt.Println("PR Test")
 
 	// 啟動伺服器並監聽 5500 連接埠。
 	r.Run("0.0.0.0:5500")
